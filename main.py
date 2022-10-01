45fff0ae019c1278e3273950094687a280b520a0
91fabbai91 <fabian.baier@gmx.de>
2022-10-01 15:54:49 +0200

    added command line interface

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from distutils.command.config import config
-from wurzelbot.wurzelbot import Wurzelbot
-from wurzelbot.login_data import LoginData
-from wurzelbot.quest import CityQuest, ParkQuest, DecoGardenQuest
+from ast import arg
 import logging
 import yaml
 import io
+from distutils.command.config import config
+import argparse
+from wurzelbot.wurzelbot import Wurzelbot
+from wurzelbot.login_data import LoginData
+from wurzelbot.quest import CityQuest, ParkQuest, DecoGardenQuest
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Wurzelimperium Bot - the pythonic way')
+    parser.add_argument('--growPlants', default=argparse.SUPPRESS,nargs='*')
+    parser.add_argument('--farmTownPark', default=argparse.SUPPRESS, action='store_true')
+
+    args, left_overs = parser.parse_known_args()
+
     config_file = io.FileIO('config/config.yaml','r')
     config = yaml.safe_load(config_file)
     logging.basicConfig(filename=config['logging']['filename'], level=config['logging']['level'], format='%(asctime)s - %(message)s')
@@ -14,8 +23,14 @@ if __name__ == "__main__":
     wurzelbot = Wurzelbot()
     wurzelbot.start_wurzelbot(login_data)
     wurzelbot.harvest_all_garden()
-    wurzelbot.grow_anything()
+    if('growPlants' in args):
+        for plant in args.growPlants:
+            wurzelbot.grow_plants_in_gardens_by_name(plant)
+    else:
+        wurzelbot.grow_anything()
     wurzelbot.water_plants_in_all_gardens()
-    wurzelbot.collectCashFromPark()
-    wurzelbot.renew_all_items_in_park()
+
+    if('farmTownPark' in args):
+        wurzelbot.collectCashFromPark()
+        wurzelbot.renew_all_items_in_park()
     wurzelbot.stop_wurzelbot()
