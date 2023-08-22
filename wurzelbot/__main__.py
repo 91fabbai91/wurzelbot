777d64c04626094af01d537ac41b849b3ebfa671
91fabbai91 <fabian.baier@gmx.de>
2023-08-22 12:52:08 +0200

    refactoring

--- wurzelbot/__main__.py
+++ wurzelbot/__main__.py
@@ -6,36 +6,6 @@ from wurzelbot import Wurzelbot
 from config import Settings
 from login_data import LoginData
 
-#needs to get help texts and how parameters needs to be used
-class Task(BaseModel):
-    name: str
-    description: str
-
-
-    def __repr__(self) -> str:
-        return f'Name: {self.__name}, Description: {self.__description}'
-
-    def __str__(self) -> str:
-        return f'Name: {self.__name}, Description: {self.__description}'
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def description(self):
-        return self.__description
-
-class Tasks(Enum):
-    GROW_FOR_QUESTS = Task(name="grow_for_quests",description="Grow plants for quests and set the quest you want to grow for in config.yaml")
-    GROW_PLANTS = Task(name="grow_plants",description="Grow specific plants in your gardens")
-    START_BEES_TOUR = Task(name="start_bees_tour", description="Start the bees to fly out of their hive")
-    FARM_TOWN_PARK = Task(name="farm_town_park",description="To go to the Town Park, empty the cashier and renew items if possibl")
-    SELL_WIMPS_PERCENTAGE = Task(name="sell_to_wimps_percentage",description="Sell stuff to Wimps if their offer is higher than the percentage from marketplace")
-    SELL_ON_MARKETPLACE = Task(name="sell_on_marketplace",description="Sell stuff to the marketplace until the minimal amount defined here is reached")
-
-
-
 if __name__ == "__main__":  
 
     settings = Settings()
@@ -68,9 +38,3 @@ if __name__ == "__main__":
         wurzelbot.grow_anything()
     wurzelbot.water_plants_in_all_gardens()
     wurzelbot.stop_wurzelbot()
-
-class NoConfigFoundError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
