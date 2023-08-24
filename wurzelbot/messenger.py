4fed40416a856306250b27e385cae3f8202531f2
91fabbai91 <fabian.baier@gmx.de>
2023-08-24 12:07:47 +0200

    build enums for message state and folder

--- wurzelbot/messenger.py
+++ wurzelbot/messenger.py
@@ -2,24 +2,31 @@ import io
 import logging
 import re
 import xml.etree.ElementTree as eTree
+from enum import IntEnum, StrEnum
 
 import http_connection
 import message
 
-# Message States
-MSG_STATE_UNKNOWN = 1
-MSG_STATE_SENT_NO_ERR = 2
-MSG_STATE_SENT_ERR_NO_RECIPIENT = 4
-MSG_STATE_SENT_ERR_NO_SUBJECT = 8
-MSG_STATE_SENT_ERR_NO_TEXT = 16
-MSG_STATE_SENT_ERR_BLOCKED = 32
-MSG_STATE_SENT_ERR_RECIPIENT_DOESNT_EXIST = 64
+
+class MessageState(IntEnum):
+    UNKNOWN = 1
+    SENT_NO_ERR = 2
+    SENT_ERR_NO_RECIPIENT = 4
+    SENT_ERR_NO_SUBJECT = 8
+    SENT_ERR_NO_TEXT = 16
+    SENT_ERR_BLOCKED = 32
+    SENT_ERR_RECIPIENT_DOESNT_EXIST = 64
+
+
+class Folder(StrEnum):
+    INBOX = "inbox"
+    SYSTEM = "system"
 
 
 class Messenger:
     def __init__(self, http_connection: http_connection.HTTPConnection):
         self.__http_connection = http_connection
-        self.__logger = logging.getLogger()
+        self.__logger = logging.getLogger(self.__class__.__name__)
         self.__sent = []
 
     def __get_new_message_id(self):
@@ -47,18 +54,18 @@ class Messenger:
         number_of_unknown_messages = 0
 
         error_mask = (
-            MSG_STATE_SENT_ERR_BLOCKED
-            | MSG_STATE_SENT_ERR_NO_RECIPIENT
-            | MSG_STATE_SENT_ERR_NO_SUBJECT
-            | MSG_STATE_SENT_ERR_NO_TEXT
-            | MSG_STATE_SENT_ERR_RECIPIENT_DOESNT_EXIST
+            MessageState.SENT_ERR_BLOCKED.value
+            | MessageState.SENT_ERR_NO_RECIPIENT
+            | MessageState.SENT_ERR_NO_SUBJECT
+            | MessageState.SENT_ERR_NO_TEXT
+            | MessageState.SENT_ERR_RECIPIENT_DOESNT_EXIST
         )
 
         for msg in self.__sent:
-            if msg.state & MSG_STATE_SENT_NO_ERR != 0:
+            if msg.state & MessageState.SENT_NO_ERR != 0:
                 number_of_successful_messages += 1
 
-            elif msg.state & MSG_STATE_UNKNOWN != 0:
+            elif msg.state & MessageState.UNKNOWN != 0:
                 number_of_unknown_messages += 1
 
             elif msg.state & error_mask != 0:
@@ -102,15 +109,15 @@ class Messenger:
             counter += 1
             self.__logger.debug(f"{counter} von {number_recipients}")
 
-    def read_message(self):
+    def read_message(self, folder: Folder):
         # Parses messages from inbox to python objects
         # and level up messages can so be read from wurzelbot
         content = self.__http_connection.execute_message_command(
-            "list.php?folder=inbox"
+            "list.php?folder={folder.value}"
         )
         content = content.decode("UTF-8")
         html = bytearray(content, encoding="UTF-8")
-        parse_inbox_from_html(html)
+        self.__logger.info(parse_inbox_from_html(html))
 
 
 def parse_inbox_from_html(html: str):
@@ -121,7 +128,7 @@ def parse_inbox_from_html(html: str):
     html_tree = eTree.parse(html_file)
     root = html_tree.getroot()
     table = root.find("./body/table")
-    print(table)
+    return table
 
 
 def blocked_from_message_recipient(result):
@@ -190,25 +197,25 @@ def get_message_delivery_state(result):
     """
     state = 0
     if was_delivery_successful(result) is True:
-        state |= MSG_STATE_SENT_NO_ERR
+        state |= MessageState.SENT_NO_ERR
     else:
         if did_the_message_recipient_exist(result) is False:
-            state |= MSG_STATE_SENT_ERR_RECIPIENT_DOESNT_EXIST
+            state |= MessageState.SENT_ERR_RECIPIENT_DOESNT_EXIST
 
         if did_the_message_had_a_subject(result) is False:
-            state |= MSG_STATE_SENT_ERR_NO_SUBJECT
+            state |= MessageState.SENT_ERR_NO_SUBJECT
 
         if did_the_message_had_a_text(result) is False:
-            state |= MSG_STATE_SENT_ERR_NO_TEXT
+            state |= MessageState.SENT_ERR_NO_TEXT
 
         if did_the_message_had_a_recipient(result) is False:
-            state |= MSG_STATE_SENT_ERR_NO_RECIPIENT
+            state |= MessageState.SENT_ERR_NO_RECIPIENT
 
         if blocked_from_message_recipient(result) is True:
-            state |= MSG_STATE_SENT_ERR_BLOCKED
+            state |= MessageState.SENT_ERR_BLOCKED
 
     if state == 0:
-        state = state or MSG_STATE_UNKNOWN
+        state = state or MessageState.UNKNOWN
 
     return state
 
