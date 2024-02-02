6bab38c87d53fe410be8402c19de0333f06ccbd7
91fabbai91 <fabian.baier@gmx.de>
2024-02-02 07:00:29 +0100

    improved honey farming

--- wurzelbot/bees_farm.py
+++ wurzelbot/bees_farm.py
@@ -48,9 +48,10 @@ class BeesFarm:
     def start_all_bees_tour(self, tour: BeesTour):
         jcontent = self.__go_to_bees()
         if jcontent["data"]["stock"] != []:
-            if int(list(jcontent["data"]["stock"].values())[0]) == 100000:
-                self.__http_connection.execute_command(f"do=bees_fill")
-                self.__logger.debug("Got Honey")
+            for entry in list(jcontent["data"]["stock"].values()):
+                if int(entry) == 100000:
+                    self.__http_connection.execute_command(f"do=bees_fill")
+                    self.__logger.debug("Got Honey")
 
         for index, hive in enumerate(self.__hives):
             if "tour_remain" not in hive:
